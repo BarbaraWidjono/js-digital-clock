2f4d9d5e082091e16a68a9b8a6087ee06851ae97
BarbaraWidjono <barbarawidjono@gmail.com>
2018-11-16 15:45:04 -0800

    clock setInterval function

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
+
+
 $(document).ready(() => {
-  let target = $('#clock');
-  target.html($('<p>clock modified with jquery<p>'));
+  // let time = function time(){
+  //   return Date.now();
+  // };
+  //
+  // let newTime = time();
+  //
+  //
+  // let target = $('#clock');
+  // target.html($(`<p>${newTime}<p>`));
+
+  setInterval(changeTime,1000);
+
+  function changeTime(){
+    let time = function time(){
+      return Date.now();
+    };
+
+    let newTime = time();
+
+
+    let target = $('#clock');
+    target.html($(`<p>${newTime}<p>`));
+  }
 
 
 });
